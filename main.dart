4603ad0624734cb6b77b5133900bfe990f2ae837
장연우 <jangyw0927@naver.com>
2024-12-30 11:35:44 +0900

    연승 로직 구현
    사용자 연승 시 게임 지속

--- main.dart
+++ main.dart
@@ -3,23 +3,38 @@ import 'dart:io';
 import 'dart:math';
 
 void main() {
+  int winCnt = 0;
+
   // 1. 사용자에게 어떤 타입을 낼 것인지 물어보는 질문 창
+  // 사용자 연승 시 계속해서 진행
+  while (true) {
+    print('가위, 바위, 보 중 하나를 정하여 입력해주세요 : ');
+    final String userInput = stdin.readLineSync(encoding: utf8) ?? 'Error';
+    // ?? => Null값 입력 시 출력
 
-  print('가위, 바위, 보 중 하나를 정하여 입력해주세요 : ');
-  final String userInput = stdin.readLineSync(encoding: utf8) ?? 'Error';
-  // ?? => Null값 입력 시 출력
+    // 2. 컴퓨터가 무엇을 낼지 결정하는 함수
+    const selectList = ['가위', '바위', '보'];
+    final cpuInput = selectList[Random().nextInt(3)];
 
-  // 2. 컴퓨터가 무엇을 낼지 결정하는 함수
-  const selectList = ['가위', '바위', '보'];
-  final cpuInput = selectList[Random().nextInt(3)];
+    // 3. 사용자와 컴퓨터간의 가위바위보 결과를 계산할 함수
+    String result = getResult(userInput, cpuInput);
 
-  // 3. 사용자와 컴퓨터간의 가위바위보 결과를 계산할 함수
+    if (result.contains('올바른')) {
+      print(result);
+      continue;
+    } else {
+      print('CPU가 $cpuInput를 냈습니다.');
+      print(result);
+    }
 
-  print('CPU가 $cpuInput를 냈습니다.');
-
-
-  String result = getResult(userInput, cpuInput);
-  print(result);
+    if (result.contains('Player')) {
+      winCnt++;
+      print("사용자가 $winCnt 연승중입니다.");
+    } else if (result.contains('CPU')) {
+      winCnt = 0;
+      break;
+    }
+  }
 }
 
 String getResult(String userInput, String cpuInput) {
@@ -29,7 +44,8 @@ String getResult(String userInput, String cpuInput) {
 
   String result = draw;
 
-  switch (userInput) {  // 사용자 입력에 대한 결과 검증
+  switch (userInput) {
+    // 사용자 입력에 대한 결과 검증
     case '가위':
       if (cpuInput == '바위') {
         result = cpuWin;
@@ -54,7 +70,7 @@ String getResult(String userInput, String cpuInput) {
       } else {
         result = draw;
       }
-    default : // 입력값이 위의 케이스 중 없을 경우
+    default: // 입력값이 위의 케이스 중 없을 경우
       result = '올바른 값을 입력해주세요.';
   }
   return result;
